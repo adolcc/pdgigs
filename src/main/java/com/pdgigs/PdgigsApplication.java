486619c5b79ffe3075aa3d1a4589e5445163afab
Adolfo Caicaguare <caicaguareadolfo@gmail.com>
2025-11-06 22:31:20 +0100

    feat: setup

---
+++ src/main/java/com/pdgigs/PdgigsApplication.java
@@ -0,0 +1,13 @@
+package com.pdgigs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdgigsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdgigsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdgigs/PdgigsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdgigs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdgigsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
